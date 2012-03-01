38d67f471c49eb05990420c71aca6599e8c79f43
Brandon John-Freso <brandonjf@atomicflowtech.com>
2012-02-29 20:39:56 -0500

    Commiting changes to the index

--- app/index.js
+++ app/index.js
@@ -1,24 +1,6 @@
 (function() {
-  var ProjectView, TaskItemView,
-    __hasProp = Object.prototype.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; },
-    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-
-  $('#startEveLink').click(function() {
-    return window.Eve = new EveEngine;
-  });
-
-  $('#createProjectLink').click(function() {
-    return Eve.createProject();
-  });
-
-  $('#renderProjectLink').click(function() {
-    return Eve.renderProject();
-  });
-
-  $('#createTaskLink').click(function() {
-    return Eve.createTask();
-  });
+  var __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
   this.EveEngine = (function(_super) {
 
@@ -28,50 +10,17 @@
       EveEngine.__super__.constructor.apply(this, arguments);
     }
 
-    EveEngine.prototype.defaults = {
-      "status": "EVE ENGINE IS OFFLINE",
-      "notification": null
-    };
-
     EveEngine.prototype.initialize = function() {
       this.set({
         ProjectList: []
       });
       $("#eveStatusHeader").html("<div>EVE ENGINE IS ONLINE</div>");
       Cufon.refresh();
-      this.set({
-        "status": "EVE ENGINE IS ONLINE"
-      });
       return console.log(this);
     };
 
-    EveEngine.prototype.setStatus = function(status) {
-      this.set({
-        "status": status
-      });
-      status = Eve.get('status');
-      $("#eveStatusHeader").html("<div>" + status + "</div>");
-      return Cufon.refresh();
-    };
-
-    EveEngine.prototype.notify = function(notification) {
-      $("#eveStatusHeader").html("<div>" + notification + "</div>");
-      Cufon.refresh();
-      return setTimeout((function() {
-        $("#eveStatusHeader").html("<div>" + (Eve.get('status')) + "</div>");
-        return Cufon.refresh();
-      }), 2000);
-    };
-
     EveEngine.prototype.createProject = function() {
-      return Eve.get("ProjectList").push(new Project);
-    };
-
-    EveEngine.prototype.renderProject = function() {
-      var projectview;
-      return projectview = new ProjectView({
-        model: Eve.get('ProjectList')[0]
-      });
+      return this.ProjectAlpha = new Project;
     };
 
     return EveEngine;
@@ -109,122 +58,32 @@
     Project.prototype.url = "http://devdashapi.atomicflowtech.com/api/tasks";
 
     Project.prototype.defaults = {
-      "ProjectName": "Project Name Not Set"
+      name: "Project Name Not Set"
     };
 
     Project.prototype.model = Task;
 
     Project.prototype.initialize = function() {
-      console.log(this);
-      Eve.notify("New project created...");
-      console.log("The new Project " + this.ProjectName);
+      this.name = prompt("What is the name of the project?");
+      console.log("New Project Created");
+      Eve.get("ProjectList").push(this);
       return this.fetch();
     };
 
     return Project;
 
-  })(Backbone.Collection.extend({
-    "ProjectName": "projectName"
-  }));
+  })(Backbone.Collection);
 
-  ProjectView = (function(_super) {
+  $('#startEveLink').click(function() {
+    return window.Eve = new EveEngine;
+  });
 
-    __extends(ProjectView, _super);
+  $('#createProjectLink').click(function() {
+    return Eve.createProject();
+  });
 
-    function ProjectView() {
-      this._click = __bind(this._click, this);
-      ProjectView.__super__.constructor.apply(this, arguments);
-    }
-
-    ProjectView.prototype.className = 'ProjectViewClass';
-
-    ProjectView.prototype.tagName = 'div';
-
-    ProjectView.prototype.id = "ProjectViewDiv";
-
-    ProjectView.prototype.el = $("#dataDisplayContent");
-
-    ProjectView.prototype.events = {
-      click: '_click'
-    };
-
-    ProjectView.prototype.initialize = function() {
-      Eve.notify("Rendering project...");
-      this.render();
-      this.model.bind('change', this.render, this);
-      return this.model.bind('reset', this.render, this);
-    };
-
-    ProjectView.prototype.render = function() {
-      var Project, RenderTarget;
-      RenderTarget = this.el;
-      console.log("attempting render");
-      console.log(this.model);
-      Project = this.model;
-      Project.each(function(task) {
-        var html;
-        task = task.toJSON();
-        html = ich.taskListItemTemplate(task);
-        return $(RenderTarget).append(html);
-      });
-      return this;
-    };
-
-    ProjectView.prototype._click = function() {
-      return console.log(this.model);
-    };
-
-    return ProjectView;
-
-  })(Backbone.View);
-
-  TaskItemView = (function(_super) {
-
-    __extends(TaskItemView, _super);
-
-    function TaskItemView() {
-      this._click = __bind(this._click, this);
-      TaskItemView.__super__.constructor.apply(this, arguments);
-    }
-
-    TaskItemView.prototype.className = 'taskListItem';
-
-    TaskItemView.prototype.tagName = 'div';
-
-    TaskItemView.prototype.id = "taskItem" + Math.random();
-
-    TaskItemView.prototype.el = $("#dataDisplayContent");
-
-    TaskItemView.prototype.events = {
-      click: '_click'
-    };
-
-    TaskItemView.prototype.initialize = function() {
-      this.model.bind('change', this.render, this);
-      return this.model.bind('reset', this.render, this);
-    };
-
-    TaskItemView.prototype.render = function() {
-      var Project, RenderTarget;
-      RenderTarget = this.el;
-      console.log("attempting render");
-      console.log(this.model);
-      Project = this.model;
-      Project.each(function(task) {
-        var html;
-        task = task.toJSON();
-        html = ich.taskListItemTemplate(task);
-        return $(RenderTarget).append(html);
-      });
-      return this;
-    };
-
-    TaskItemView.prototype._click = function() {
-      return console.log(this.model);
-    };
-
-    return TaskItemView;
-
-  })(Backbone.View);
+  $('#createTaskLink').click(function() {
+    return Eve.createTask();
+  });
 
 }).call(this);
